d320707ff6360131179bde748bb24a21f8efdbff
Sithum Imesh <sithum.imesh052002@icloud.com>
2024-08-25 09:12:30 +0530

    Initial Back End

---
+++ Internship_Program/back-end/src/main/java/com/senturatechnologies/internshipprogram/backend/BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.senturatechnologies.internshipprogram.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+
+}
---
+++ Internship_Program/back-end/src/test/java/com/senturatechnologies/internshipprogram/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senturatechnologies.internshipprogram.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
